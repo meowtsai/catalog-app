b64c47b67cfa9f288af30cb36191ec0283271d25
meowtsai <shihfan.tsai@gmail.com>
2017-11-03 17:31:04 +0800

    integrate wsgi

---
+++ catalog.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/html/catalog-app')
+
+from project import app as application
--- project.py
+++ project.py
@@ -8,6 +8,7 @@ from flask import (
                     jsonify)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from models import Category, Base, Product, User
 from flask import session as login_session
 import random
@@ -19,18 +20,26 @@ import json
 from flask import make_response
 from google.oauth2 import id_token
 from google.auth.transport import requests
+import os.path
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///categoryproduct.db')
+engine = create_engine('sqlite:////var/www/html/catalog-app/categoryproduct.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+session = scoped_session(DBSession)
 
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+    open('/var/www/html/catalog-app/client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Catalop Application"
+
+app.secret_key = 'super_secret_key'
+
+
+@app.teardown_request
+def remove_session(ex=None):
+    session.remove()
 
 
 @app.route('/login')
@@ -278,6 +287,6 @@ def deleteCatalogItem(category_id, item_id):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
